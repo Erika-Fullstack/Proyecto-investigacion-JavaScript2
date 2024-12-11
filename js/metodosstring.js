e0908b63a649827ab5db0e9043fde843d62dc439
Erika-Fullstack <erika.full.stack@gmail.com>
2024-12-11 13:59:16 +0100

    cambios en ejemplos de strings html y js

---
+++ js/metodosstring.js
@@ -0,0 +1,50 @@
+/* .length */
+let texto = "Hola Mundo"; 
+console.log(texto.length); // 10
+
+/* .toUpperCase() */
+let texto1 = "Hola"; 
+console.log(texto1.toUpperCase()); // "HOLA" 
+
+/* .toLowerCase() */
+let texto2 = "Hola"; 
+console.log(texto2.toLowerCase()); // "hola"
+
+/* .trim() */
+let texto3 = " Hola "; 
+console.log(texto3.trim()); // "Hola"
+
+/* .indexOf */
+let texto4 = "Hola Mundo"; 
+console.log(texto4.indexOf("Mundo")); // 5 
+
+/* includes */
+console.log("Hola Mundo".includes("Mundo")); // true
+
+/* startsWith(searchValue, start) */
+let cadena = "Hola, mundo";
+let cadenaDeBusqueda = "mundo";
+let resultado = cadena.startsWith(cadenaDeBusqueda, 7);
+console.log(resultado); // Imprime: true
+
+/* startsWith(searchValue, start) */
+let cadena2 = "Hola, mundo";
+let cadenaDeBusqueda2 = "mundo";
+let resultado2 = cadena2.startsWith(cadenaDeBusqueda2, 7);
+console.log(resultado2); // Imprime: true
+
+/* .replace(searchValue, newValue) */
+let texto5 = "Hola Mundo"; 
+console.log(texto5.replace("Mundo", "Amigo")); // "Hola Amigo"
+
+/* split(separator) */
+let texto6 = "Hola,Mundo"; 
+console.log(texto6.split(",")); // ["Hola", "Mundo"]
+
+/* .charAt(index) */
+let texto7 = "hola"; 
+console.log(texto7.charAt(1)); // "o"
+
+/* [index] */
+let texto8 = "hola"; 
+console.log(texto8[1]); // "o"
