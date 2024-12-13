24facb445b62b04328ecf6c55574ef8d84745de5
Erika-Fullstack <erika.full.stack@gmail.com>
2024-12-13 11:18:59 +0100

    cambios en ejemplos de string

--- js/metodosstring.js
+++ js/metodosstring.js
@@ -1,49 +1,49 @@
 /* .length */
 let texto = "Hola Mundo"; 
-console.log(texto.length); // 10
+//console.log(texto.length);  10
 
 /* .toUpperCase() */
 let texto1 = "Hola"; 
-console.log(texto1.toUpperCase()); // "HOLA" 
+//console.log(texto1.toUpperCase());  "HOLA" 
 
 /* .toLowerCase() */
 let texto2 = "Hola"; 
-console.log(texto2.toLowerCase()); // "hola"
+//console.log(texto2.toLowerCase());  "hola"
 
 /* .trim() */
 let texto3 = " Hola "; 
-console.log(texto3.trim()); // "Hola"
+// console.log(texto3.trim()); "Hola"
 
 /* .indexOf */
 let texto4 = "Hola Mundo"; 
-console.log(texto4.indexOf("Mundo")); // 5 
+//console.log(texto4.indexOf("Mundo"));  5 
 
 /* includes */
-console.log("Hola Mundo".includes("Mundo")); // true
+//console.log("Hola Mundo".includes("Mundo"));  true
 
 /* startsWith(searchValue, start) */
 let cadena = "Hola, mundo";
 let cadenaDeBusqueda = "mundo";
 let resultado = cadena.startsWith(cadenaDeBusqueda, 7);
-console.log(resultado); // Imprime: true
+// console.log(resultado); Imprime: true
 
 /* lastIndexOf() */
 const animales = ['perro', 'gato', 'pez', 'perro', 'pájaro'];
 const ultimoPerro = animales.lastIndexOf('perro');
-console.log(ultimoPerro); // Imprime: 3
+//console.log(ultimoPerro);  Imprime: 3
 
 /* .replace(searchValue, newValue) */
 let texto5 = "Hola Mundo"; 
-console.log(texto5.replace("Mundo", "Amigo")); // "Hola Amigo"
+// console.log(texto5.replace("Mundo", "Amigo")); "Hola Amigo"
 
 /* split(separator) */
 let texto6 = "Hola,Mundo"; 
-console.log(texto6.split(",")); // ["Hola", "Mundo"]
+// console.log(texto6.split(",")); ["Hola", "Mundo"]
 
 /* .charAt(index) */
 let texto7 = "hola"; 
-console.log(texto7.charAt(1)); // "o"
+//console.log(texto7.charAt(1));  "o"
 
 /* [index] */
 let texto8 = "hola"; 
-console.log(texto8[1]); // "o"
+//console.log(texto8[1]);  "o"
