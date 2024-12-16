8b23e1b0021fd624ac9bc615641823f8a7c4fe8c
gaizkamg <gaizkamg@gmail.com>
2024-12-16 14:21:29 +0100

    metodos arrays

--- js/metodosarrays.js
+++ js/metodosarrays.js
@@ -3,6 +3,10 @@ let letras = ["b", "a", "c"];
 let frutas = ["manzana", "banana", "naranja"];
 let frutas2 = ["banana", "uva"];
 
+let letras = ["b", "a", "c"]; 
+letras.sort();
+// ["a", "b", "c"]
+
 
 /* .length 
 console.log(numeros.length); 
@@ -21,22 +25,18 @@ numeros.shift();
 // [2, 3]
 
 /* .sort() 
-
 console.log(letras.sort());
 // ["a", "b", "c"]
 
 /* .reverse() 
-
 numeros.reverse(); 
 // [3, 2, 1]
 
 /* .slice() 
-
 let seleccion = frutas.slice(0, 2);
 // ["manzana", "banana"]
 
 /* .concat() 
-
 let todas = frutas1.concat(frutas2);
 // ["manzana", "banana", "banana", "uva"]
 
@@ -51,12 +51,12 @@ frutas.splice(2, 0, "Limón", "Kiwi");
 
 seleccion = frutas.splice(2, 2); 
 // En la posicion 2 borra 2 elementos.
-console.log(seleccion)                              // nos devuelve un array con lo borrado
+console.log(seleccion)  // nos devuelve un array con lo borrado
 // ["Limón", "Kiwi"]
 
 
 /* .lastIndexof() */
-
+frutas2 = ["manzana", "banana", "banana", "naranja"];
 frutas2.lastIndexOf("banana"); 
 // 2
 
